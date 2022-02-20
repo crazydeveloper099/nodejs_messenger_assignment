15a7064e0258f5eea350c7163f5263e544749842
Anubhav Mehrotra (Group) <anubhav.mehrotra@global.ntt>
2022-02-20 23:18:36 +0530

    initial node app

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import {getIndex} from "../controllers/indexController.js";
-
+import {getWebhook, postWebhook} from "../controllers/messengerController.js";
 let expressRouter = express.Router();
 
 export const initRoutes=(app)=>{
     expressRouter.get("/",getIndex);
-
+    expressRouter.get("/webhook",getWebhook);
+    expressRouter.post("/webhook",postWebhook);
     return app.use("/",expressRouter);
 }
 
