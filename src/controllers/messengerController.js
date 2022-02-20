065d3f89a4245e3e71a43b8ffd98fcf12502f537
Anubhav Mehrotra (Group) <anubhav.mehrotra@global.ntt>
2022-02-21 00:05:15 +0530

    messenger functions integration

--- src/controllers/messengerController.js
+++ src/controllers/messengerController.js
@@ -2,26 +2,32 @@ import 'dotenv/config';
 
 
 export const postWebhook =(req,res)=>{ 
-        let body = req.body;
+    // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+        // Gets the body of the webhook event
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
       
-        // Checks this is an event from a page subscription
-        if (body.object === 'page') {
-      
-          // Iterates over each entry - there may be multiple if batched
-          body.entry.forEach(function(entry) {
-      
-            // Gets the message. entry.messaging is an array, but 
-            // will only ever contain one message, so we get index 0
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-          });
-      
-          // Returns a '200 OK' response to all requests
-          res.status(200).send('EVENT_RECEIVED');
-        } else {
-          // Returns a '404 Not Found' if event is not from a page subscription
-          res.sendStatus(404);
-        }
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
 }
 
 export const getWebhook =(req,res)=>{
@@ -50,4 +56,20 @@ export const getWebhook =(req,res)=>{
             res.sendStatus(403);      
           }
         }
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
+
 }
